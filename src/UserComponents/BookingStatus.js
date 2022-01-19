c1efed4e98bb90e28fda9189e5b19dca53752ba8
Dharanikumar19 <dharanikumarmuthusamy@gmail.com>
2022-01-19 12:50:18 +0530

    booking

--- src/UserComponents/BookingStatus.js
+++ src/UserComponents/BookingStatus.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function BookingStatus() {
   return (
   <>
-             <Link to={"/userdashboard"} className='btn btn-outline-primary' style={{position: "absolute", top:"20px", left:"10px"}}>Go Back</Link>
+             <Link to={"/userdashboard"} className='btn btn-outline-primary' style={{position: "absolute", top:"10px", left:"10px"}}>Go Back</Link>
     <h4 className='mt-5 ml-5'> Currently your Booking Status is Empty...</h4>
     </>
    )
